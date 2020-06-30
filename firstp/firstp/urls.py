eb234b5be118cac897451db92ad5e377ba016eeb
dayton404 <dayton@blocgamers.com>
2020-06-29 23:22:20 -0600

    Update urls.py
    Fixed path for login and logout

--- firstp/firstp/urls.py
+++ firstp/firstp/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('/accounts/login',LoginView.as_view(),name='login'),
-    path('/accounts/logout',LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
+    path('accounts/login',LoginView.as_view(),name='login'),
+    path('accounts/logout',LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
 ]
